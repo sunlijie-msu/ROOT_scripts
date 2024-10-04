4c70befc4c39037651d9ac0c26b031ca1d9f48e3
Lijie Sun <47992432+sunlijie-msu@users.noreply.github.com>
2024-10-04 00:07:33 -0400

    Update Tfit.C

--- Tfit.C
+++ Tfit.C
@@ -352,6 +352,13 @@ void print_histogram() {
 }
 //usage: root -l -q print_histogram.C > h1.log
 
+TFile* _file0 = TFile::Open("S31_Gamma4156_Eg4155.84_Tau0.0_SP1.20_AC0.0_all.root");
+TH1F* Eg = new TH1F("Eg", "Eg", 8000, 0., 8000);
+tree->Draw("Clovere>>Eg", "Clovere>0&&Clovere<8000&&DSSD1e+DSSD2e>10000&&DSSD1e>100&&DSSD2e>100", "");
+TFile* fout = new TFile("S31_Gamma4156_Eg4155.84_Tau0.0_SP1.20_AC0.0.root", "RECREATE");
+Eg->Write();
+fout->Close();
+
 T888->GetEntries()
 T888->Show(283)
 for( Int_t i=1;i<h1->GetNbinsX();i++)
