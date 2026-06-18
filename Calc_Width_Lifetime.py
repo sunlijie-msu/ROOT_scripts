0c92dca82f455fa76e60a899bb0b2f64b80aeb23
Lijie Sun <47992432+sunlijie-msu@users.noreply.github.com>
2026-06-18 17:11:13 -0400

    Update half-life value in Calc_Width_Lifetime.py for improved decay width calculation and add Calc_Weighted_Average.xlsx for data analysis

--- Calc_Width_Lifetime.py
+++ Calc_Width_Lifetime.py
@@ -3,7 +3,7 @@ from uncertainties.umath import log
 
 hbar = 6.582119569e-16 # eV*s
 half_life = ufloat(68.03e-9, 0.07e-9) # s
-half_life = ufloat(67.90e-9, 0.07e-9) # s
+half_life = ufloat(67.895e-9, 0.066e-9) # s
 
 width = hbar / (half_life / log(2))
 
